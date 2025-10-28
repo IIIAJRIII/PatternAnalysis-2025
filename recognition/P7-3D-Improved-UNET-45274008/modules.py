5468618eadb3eca679d6f9755ab8c82eff5c191d
AJR <alanravikumar.ajr@gmail.com>
2025-10-28 18:11:11 +1000

    Updated modules to make it simpler

--- recognition/P7-3D-Improved-UNET-45274008/modules.py
+++ recognition/P7-3D-Improved-UNET-45274008/modules.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class DoubleConv3D(nn.Module):
-    """(Convolution -> Batch Normalization -> ReLU) * 2"""
+# --- Building Blocks ---
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
@@ -28,7 +31,7 @@ class Down(nn.Module):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool3d(2),
-            DoubleConv3D(in_channels, out_channels)
+            DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x):
@@ -44,34 +47,27 @@ class Up(nn.Module):
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
-            self.conv = DoubleConv3D(in_channels, out_channels, in_channels // 2)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
             self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv3D(in_channels, out_channels)
+            self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
-        # x1 is from the upsampling path, x2 is the skip connection
         x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        diffZ = x2.size()[4] - x1.size()[4] # 3D difference
 
-        # Match the spatial dimensions of the skip connection tensor x2
-        # Input tensors (x1, x2) are expected to be in (N, C, D, H, W) format
-        diffZ = x2.size()[2] - x1.size()[2]
-        diffY = x2.size()[3] - x1.size()[3]
-        diffX = x2.size()[4] - x1.size()[4]
-
-        # Apply padding to x1 to match x2's dimensions
-        p3d = (diffX // 2, diffX - diffX // 2,
-               diffY // 2, diffY - diffY // 2,
-               diffZ // 2, diffZ - diffZ // 2)
-        x1 = nn.functional.pad(x1, p3d)
+        x1 = F.pad(x1, [diffZ // 2, diffZ - diffZ // 2,
+                        diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
         
-        # Concatenate along the channel dimension
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
 class OutConv(nn.Module):
-    """Final 1x1x1 convolution to map to the number of output classes"""
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
         self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
@@ -79,43 +75,42 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+# --- Full 3D UNet Model ---
+
 class UNet3D(nn.Module):
-    """
-    The main 3D UNet model.
-    """
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    # Updated to accept base_features, defaulting to 32
+    def __init__(self, n_channels, n_classes, base_features=32, bilinear=False):
         super(UNet3D, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        # Encoder path
-        self.inc = DoubleConv3D(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
+        self.inc = DoubleConv(n_channels, base_features)
+        self.down1 = Down(base_features, base_features * 2)
+        self.down2 = Down(base_features * 2, base_features * 4)
+        self.down3 = Down(base_features * 4, base_features * 8)
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-
+        # The bottleneck layer
+        self.down4 = Down(base_features * 8, (base_features * 16) // factor) 
+        
         # Decoder path
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.up1 = Up(base_features * 16, (base_features * 8) // factor, bilinear)
+        self.up2 = Up(base_features * 8, (base_features * 4) // factor, bilinear)
+        self.up3 = Up(base_features * 4, (base_features * 2) // factor, bilinear)
+        self.up4 = Up(base_features * 2, base_features, bilinear) # No factor on last 'out_channels'
+        self.outc = OutConv(base_features, n_classes)
 
     def forward(self, x):
-        # Encoder
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         
-        # Decoder
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
+
